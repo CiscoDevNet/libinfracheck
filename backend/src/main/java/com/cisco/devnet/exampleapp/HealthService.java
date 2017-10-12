4a97aa19428de3f2e203a5e87e55dfd93f9297fe
Ashley Roach <asroach@cisco.com>
2017-10-12 14:07:33 -0600

    Add health endpoint and integrate libinfracheck

--- backend/src/main/java/com/cisco/devnet/exampleapp/HealthService.java
+++ backend/src/main/java/com/cisco/devnet/exampleapp/HealthService.java
@@ -4,19 +4,41 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import com.cisco.devnet.infracheck.InfraCheck;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 
 @Path("/")
 public class HealthService {
     @GET
-    @Path("/{param}")
-    public Response getMessage(@PathParam("param") String message) {
-        String output = "Jersey says " + message;
+    @Path("/health")
+    public Response getHealth(@PathParam("param") String message) {
+
+        String healthStatus;
+        HttpResponse<JsonNode> healthObject;
         InfraCheck health = new InfraCheck();
+
         try {
-            output = health.getHealth().toString();
+
+            String token = health.login().getBody().getObject().getJSONObject("response").getString("serviceTicket");
+
+            healthObject = health.pathCheck(token);
+
+            healthStatus = healthObject
+                    .getBody()
+                    .getObject()
+                    .getJSONObject("response")
+                    .getJSONObject("request")
+                    .getString("status");
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return Response.status(200).entity(output).build();
+
+        if ( healthStatus.equals("FAILED") ) {
+            return Response.status(500).entity(healthObject.getBody().toString()).build();
+        } else {
+            return Response.status(200).entity("OK").build();
+        }
+
     }
 }
